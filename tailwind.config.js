2a5d864b38b5faf32859bb85ec2c92909844def0
anthonyRasband <anthonyrasband2939@mtec.edu.stu>
2023-09-13 14:46:38 -0600

    did tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.{htm1,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
